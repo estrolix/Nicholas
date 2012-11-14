2f04e59514dc0cc4334cda4255f2f766c9afc895
Volodymyr Vavrykovych <estrolix@gmail.com>
2012-11-14 18:58:36 +0200

    changed db config

--- app/config/database.php
+++ app/config/database.php
@@ -6,8 +6,8 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'control1system',
-		'database' => 'nikolaos',
+		'password' => 'root',
+		'database' => 'nicholas',
 	);
 }
 ?>
